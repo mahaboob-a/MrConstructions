4ee799c28dd24ff377c2adb21010d0d8615113d0
mahaboob-a <shaikmehaboob732@gmail.com>
2026-09-11 10:23:18 +0530

    Prepare backend for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -48,9 +48,12 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-
 connectDB().then(() => {
-  app.listen(PORT, () => console.log(`[server] MR Constructions API running on port ${PORT}`));
+  app.listen(Number(PORT), "0.0.0.0", () => {
+    console.log(
+      `[server] MR Constructions API running on port ${PORT}`
+    );
+  });
 });
 
 export default app;
